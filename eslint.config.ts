44ed6acf87be04c4aedf5d5d0c1982d903fc205f
Sebastian Fastner <s.fastner@sebastian-software.de>
2026-03-13 09:19:45 +0100

    chore: add explicit type annotation to ESLint config export

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,10 @@
+import type { Linter } from "eslint"
+
 import { getEslintConfig } from "eslint-config-setup"
 
 const config = await getEslintConfig({ ai: true, node: true })
 
-export default [
+const eslintConfig: Linter.Config[] = [
   ...config,
   {
     ignores: [
@@ -17,3 +19,5 @@ export default [
     ],
   },
 ]
+
+export default eslintConfig
