849e04384e6bd4862109beb1f29c9db10d2598f7
Sebastian Fastner <s.fastner@sebastian-software.de>
2026-03-15 17:55:25 +0100

    chore: suppress non-literal-fs-filename lint for static config file reads

--- packages/paratix/tsup.config.ts
+++ packages/paratix/tsup.config.ts
@@ -3,6 +3,7 @@ import { resolve } from "node:path"
 import { defineConfig } from "tsup"
 
 const packageJsonPath = resolve(import.meta.dirname, "package.json")
+// eslint-disable-next-line security/detect-non-literal-fs-filename -- build-time config: path is statically resolved from import.meta.dirname
 const packageJson: unknown = JSON.parse(readFileSync(packageJsonPath, "utf8"))
 // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion -- build-time config: package.json always has version
 const { version } = packageJson as { version: string }
