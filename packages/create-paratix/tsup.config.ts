cf8e25a022099f623153e615be4232b09104b257
Sebastian Fastner <s.fastner@sebastian-software.de>
2026-03-15 15:41:04 +0100

    chore: enable declaration file generation for create-paratix

--- packages/create-paratix/tsup.config.ts
+++ packages/create-paratix/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   banner: { js: "#!/usr/bin/env node" },
   clean: true,
+  dts: true,
   entry: ["src/index.ts"],
   format: ["esm"],
   target: "node24",
